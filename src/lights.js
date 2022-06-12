81daed376f8d81bb48e9dd1c3d1ee9ee7eaa82bb
rootVIII <colleyloyejames@gmail.com>
2022-06-11 20:40:23 -0400

    fix lighting/shadow getting clipped

--- src/lights.js
+++ src/lights.js
@@ -4,8 +4,12 @@ class Lights {
     constructor() {
         this.ambientLight = new AmbientLight('white', 1);
         this.directionalLight = new DirectionalLight('white', 8);
-        this.directionalLight.position.set(10, 10, 10);
+        this.directionalLight.position.set(50, 50, 50);
         this.directionalLight.castShadow = true;
+        this.directionalLight.shadowCameraLeft = -10;
+        this.directionalLight.shadowCameraRight = 10;
+        this.directionalLight.shadowCameraTop = 10;
+        this.directionalLight.shadowCameraBottom = -10;
     }
 
     createAmbientLight() {
